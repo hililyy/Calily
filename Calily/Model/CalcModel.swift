c31aa81814d713141a046a2bcfefbf412748aa3d
hililyy <joun406@gmail.com>
2022-06-22 00:50:53 +0900

    commit

---
+++ Calily/Model/CalcModel.swift
@@ -0,0 +1,8 @@
+//
+//  CalcModel.swift
+//  Calily
+//
+//  Created by 강조은 on 2022/06/22.
+//
+
+import Foundation
---
+++ Calily/ViewModel/CalcViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CalcViewModel.swift
+//  Calily
+//
+//  Created by 강조은 on 2022/06/22.
+//
+
+import Foundation
