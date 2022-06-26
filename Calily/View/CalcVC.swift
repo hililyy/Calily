f39183415ff6bdce59e54a8df9f1e15ff57d52b7
hililyy <joun406@gmail.com>
2022-06-26 23:53:12 +0900

    Feat: 계산기 기능 추가

--- Calily/View/CalcVC.swift
+++ Calily/View/CalcVC.swift
@@ -6,6 +6,13 @@
 //
 
 import UIKit
+enum Operation {
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case unknown
+}
 
 class CalcVC: UIViewController {
 
@@ -19,27 +26,87 @@ class CalcVC: UIViewController {
     @IBOutlet weak var plusBtn: UIButton!
     @IBOutlet weak var resultBtn: UIButton!
     @IBOutlet weak var dotBtn: UIButton!
-    
-    var firstNum: Int?
-    var secondNum: Int?
+
     var operatorFlag: Bool = false
-    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""  // 새롭게 입력된 값을 저장하는 프로퍼티
+    var result = ""
+    var currentOpertaion: Operation = .unknown
     override func viewDidLoad() {
         super.viewDidLoad()
         resultLabel.text = "0"
     }
 
-    @IBAction func touchNumberBtn(_ sender: Any) {
+    @IBAction func touchNumberBtn(_ sender: UIButton) {
+        guard let numberValue = sender.title(for: .normal) else { return }
+        if self.displayNumber.count < 9 {
+            self.displayNumber += numberValue
+            self.resultLabel.text = self.displayNumber
+        }
     }
     
     @IBAction func touchOperatorBtn(_ sender: Any) {
+        
     }
     
+    @IBAction func touchDotBtn(_ sender: Any) {
+        if self.displayNumber.count < 8 , !self.displayNumber.contains(".") {
+            self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
+            self.resultLabel.text = self.displayNumber
+        }
+    }
     @IBAction func touchResultBtn(_ sender: Any) {
     }
     
     @IBAction func touchClearBtn(_ sender: Any) {
         resultLabel.text = "0"
+        self.displayNumber = ""
+        self.firstOperand = ""
+        self.secondOperand = ""
+        self.result = ""
+        self.currentOpertaion = .unknown
+        
     }
     
+    func operation(_ operation: Operation) {
+        if self.currentOpertaion != .unknown {
+            if !self.displayNumber.isEmpty {
+                self.secondOperand = self.displayNumber
+                self.displayNumber = ""
+                
+                guard let firstOperand = Double(self.firstOperand) else { return }
+                guard let secondOperand = Double(self.secondOperand) else { return }
+                
+                switch self.currentOpertaion {
+                case .Add:
+                    self.result = "\(firstOperand + secondOperand)"
+                    
+                case .Subtract:
+                    self.result = "\(firstOperand - secondOperand) "
+                    
+                case .Divide:
+                    self.result = "\(firstOperand / secondOperand)"
+                    
+                case .Multiply:
+                    self.result = "\(firstOperand * secondOperand)"
+                
+                default:
+                    break
+                }
+                
+                if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+                self.firstOperand = self.result
+                self.resultLabel.text = self.result
+            }
+            
+            self.currentOpertaion = operation
+        } else {
+            self.firstOperand = self.displayNumber
+            self.currentOpertaion = operation
+            self.displayNumber = ""
+        }
+    }
 }
